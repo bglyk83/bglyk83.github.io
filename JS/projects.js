cc4dbee0202f9d75d5dbd963ddfc38516822138e
Yunki <bglyk83@gmail.com>
2025-04-21 14:36:24 +0900

    portfolio 완성

--- JS/projects.js
+++ JS/projects.js
@@ -1,6 +1,6 @@
 const categories = document.querySelector(".categories");
 const projectsContainer = document.querySelector(".projects");
-const projects = document.querySelector(".project");
+const projects = document.querySelectorAll(".project");
 
 categories.addEventListener("click", (e) => {
   const filter = e.target.dataset.category;
@@ -10,10 +10,26 @@ categories.addEventListener("click", (e) => {
     return;
   }
   activeSelection(e.target);
+  filterProjects(filter);
 });
 
 function activeSelection(target) {
   const active = document.querySelector(".category--selected");
   active.classList.remove("category--selected");
   target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projects.forEach((projects) => {
+    if (filter == "all" || filter == projects.dataset.type) {
+      projects.style.display = "block";
+    } else {
+      projects.style.display = "none";
+    }
+  });
+
+  projectsContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 500);
 }
